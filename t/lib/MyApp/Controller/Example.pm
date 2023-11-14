26963bd89972f3d023359da870aa884559fa7f1c
guest20 <guest20@users.noreply.github.com>
2023-11-14 14:57:59 +0100

    Example.pm from generate lite_app

---
+++ t/lib/MyApp/Controller/Example.pm
@@ -0,0 +1,22 @@
+package # paused
+  MyApp::Controller::Example;
+use Mojolicious::Lite -signatures;
+
+get '/' => sub ($c) {
+  $c->render(template => 'index');
+};
+
+1
+__DATA__
+
+@@ index.html.ep
+%% layout 'default';
+%% title 'Welcome';
+<h1>Welcome to the Mojolicious real-time web framework!</h1>
+
+@@ layouts/default.html.ep
+<!DOCTYPE html>
+<html>
+  <head><title><%%= title %></title></head>
+  <body><%%= content %></body>
+</html>
